e08babc279065f891122352c081ab6d9372ac926
Abbey Janicek <abbey.janicek@gmail.com>
2018-08-20 01:08:26 -0500

    I moved the images folder into public. Form was working on home.html but is not working anymore.

--- server/public/scripts/controllers/rentals.controller.js
+++ server/public/scripts/controllers/rentals.controller.js
@@ -1,19 +1,33 @@
-realEstateApp.controller('RentalsController', ['$http', function($http) {
+realEstateApp.controller('RentalsController', ['$http', function ($http) {
     console.log('RentalsController GO!');
-    
+
     const self = this;
-    self.property = { list: [{ cost: '3000', sqft: '100', type: 'rental', city: 'Minneapoli', imagepath: 'rental2.jpeg' }]};
+    self.property = { list: [{ cost: '3000', sqft: '100', type: 'rental', city: 'Minneapoli', imagepath: 'rental2.jpeg' }] };
 
+    function getRental() {
+        console.log('in getRental');
+        $http({
+            method: 'GET',
+            url: '/rentals'
+        }).then(function (response) {
+            console.log('RentalsController - getRental response', response.data);
+            self.property.list = response.data;
+        }).catch(function (error) {
+            console.log('RentalsController - getRental error', error.statusText);
+        })
+    }
 
- self.deleteProperty = function (property) {
-    $http({
-        method: 'DELETE',
-        url: '/property/' + property.id
-    }).then(function (response) {
-        console.log('RentalsController - deleteRental - response');
-        getProperty();
-    }).catch(function (error) {
-        console.log('RentalsController - deleteRentals - error', error.statusText);
-    });
-}
+    getRental();
+
+    self.deleteRental = function (property) {
+        $http({
+            method: 'DELETE',
+            url: '/rentals/' + property.id
+        }).then(function (response) {
+            console.log('RentalsController - deleteRental - response');
+            getProperty();
+        }).catch(function (error) {
+            console.log('RentalsController - deleteRentals - error', error.statusText);
+        });
+    }
 }]); //end RentalsController
--- server/routes/rentals.router.js
+++ server/routes/rentals.router.js
@@ -22,6 +22,18 @@ pool.on('error', (error) => {
     console.log('error connecting to db', error);
 });
 
+router.get('/', function (req, res) {
+    console.log('in GET route');
+    const query = `SELECT * FROM "listings" WHERE "type" LIKE '%rent%';`;
+    pool.query(query).then((results) => {
+        console.log(results);
+        res.send(results.rows); 
+    }).catch((error) => {
+        console.log('error making GET', error);
+        res.sendStatus(500);  
+    });
+}) //end GET route
+
 
 
 
